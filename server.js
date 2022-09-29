fcc858d570fa9d261bd45d2c33bea803c1bb8915
cpulcini2003 <64283587+cpulcini2003@users.noreply.github.com>
2022-09-29 13:29:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-
 const app = express();
-const port = process.env.PORT || 3200;
-
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
