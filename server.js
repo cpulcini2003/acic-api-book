c2166ecbd72d6f5b3771eac0f1432b2b161d7054
Carlo Pulcini <cpulcini2003@gmail.com>
2022-09-29 12:18:31 +0200

    modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3200;
+
+let books = [];
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+  const book = req.body;
+  books.push(book);
+  //console.log(book);
+  res.json(book);
+  //res.send('Book was added to the database');
+  // res.json({
+  //   number: 1
+  // });
+  //res.send('{"success" : "Updated Successfully"' & book.id & ', "status" : 200}');
+});
+
+app.get('/books', (req, res) => {
+  res.json(books);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello from the server!');
